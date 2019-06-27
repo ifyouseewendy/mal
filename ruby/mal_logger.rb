033763bfe87282bd3861369d28fbfa3aaeb72ba7
Di Wen <ifyouseewendy@gmail.com>
2019-06-26 21:05:07 -0400

    Fix mal logger

--- ruby/mal_logger.rb
+++ ruby/mal_logger.rb
@@ -1,6 +1,6 @@
 require "logger"
 
-MAL_LOGGER ||= Logger.new("./info.log").tap do |logger|
+MAL_LOGGER ||= Logger.new(File.expand_path("./../info.log", __FILE__)).tap do |logger|
   logger.formatter = proc { |severity, datetime, progname, msg|
     "[#{severity}]: #{msg}\n"
   }
